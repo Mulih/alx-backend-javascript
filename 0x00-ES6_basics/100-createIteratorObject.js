ada6b8cfedad162683a007481dff0e7e0a69caf5
Mulih <almuih@yahoo.com>
2024-05-27 12:43:32 +0000

    add project files

---
+++ 0x00-ES6_basics/100-createIteratorObject.js
@@ -0,0 +1,29 @@
+export default function createIteratorObject(report) {
+    const departments = Object.values(report.allEmployees);
+    let currentDepartmentIndex = 0;
+    let currentEmployeeIndex = 0;
+
+    return {
+        [Symbol.iterator]() {
+            return {
+                next() {
+                    if (currentDepartmentIndex < departments.length) {
+                        const currentDepartment = departments[currentDepartmentIndex];
+                        if (currentEmployeeIndex < currentDepartment.length) {
+                            const employee = currentDepartment[currentEmployeeIndex];
+                            currentEmployeeIndex++;
+                            return { value: employee, done: false };
+                        } else {
+                            currentDepartmentIndex++;
+                            currentEmployeeIndex = 0;
+                            return this.next();
+                        }
+                    } else {
+                        return { done: true };
+                    }
+                }
+            };
+        }
+    };
+}
+
---
+++ 0x00-ES6_basics/101-iterateThroughObject.js
@@ -0,0 +1,16 @@
+export default function iterateThroughObject(reportWithIterator) {
+    let result = '';
+    const iterator = reportWithIterator;
+
+    let iterationResult = iterator.next();
+    while (!iterationResult.done) {
+        result += iterationResult.value + ' | ';
+        iterationResult = iterator.next();
+    }
+
+    
+    result = result.slice(0, -3);
+
+    return result;
+}
+
