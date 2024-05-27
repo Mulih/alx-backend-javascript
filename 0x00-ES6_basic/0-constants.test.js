aba67b813d116796921c56d0371c7040a71a40b2
Mulih <almuih@yahoo.com>
2024-05-27 18:01:53 +0000

    jest test for task 0

---
+++ 0x00-ES6_basic/0-constants.test.js
@@ -0,0 +1,16 @@
+import { taskFirst, getLast, taskNext } from './0-constants.js';
+
+describe('Task Functions', () => {
+  it('should return the correct string for taskFirst()', () => {
+    expect(taskFirst()).toBe('I prefer const when I can.');
+  });
+
+  it('should return the correct string for getLast()', () => {
+    expect(getLast()).toBe(' is okay');
+  });
+
+  it('should return the correct combined string for taskNext()', () => {
+    expect(taskNext()).toBe('But sometimes let is okay');
+  });
+});
+
