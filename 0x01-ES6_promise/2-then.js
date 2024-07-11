aadc927f84404cbd2fdc751e282ccf8515c86153
Mulih <almulih@yahoo.com>
2024-07-11 17:35:12 +0000

    upload of completed project tasks

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
-export default function handleRsponseFromAPI(Promise) {
-	return Promise
-	  .then(() => ({ status: 200, body: 'success' }))
-	  .catch(() => new Error())
-	  .finally(() => console.log('Got a response from the API'));
+export default function handleResponseFromAPI(Promise) {
+  return Promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
