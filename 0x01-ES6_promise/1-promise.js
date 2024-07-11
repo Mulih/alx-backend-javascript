dfe6a9f91ae1a7ba94bef88753871e6007c68069
Mulih <almulih@yahoo.com>
2024-07-11 09:37:05 +0000

    eslint fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(Success) {
-	return new Promise((resolve, reject) => {
-		if (Success) {
-			resolve({
-				status: 200,
-				body: 'Success',
-			});
-		} else {
-			reject(new Error('The fke API is not working currently'));
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (Success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fke API is not working currently'));
+    }
+  });
 }
