6bc8d8375725efade307c9b3fa4b6636f4065d50
Mulih <almulih@yahoo.com>
2024-07-30 03:26:55 +0000

    upload of tasks 0 - 3

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,15 @@
+/**
+ * Logs message to console.
+ * @param {string} messsage - message to be logged
+ * @return {void}
+ */
+//arrow function
+const displayMessage = (message) => console.log(message);
+
+/**
+ * exports displayMessage function
+ * @module displayMessage
+ * @function
+ */
+// default module export
+module.exports = displayMessage;
